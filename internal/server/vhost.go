9c94276478bc52d16cef8064f7e6c18fd2ea2e2c
Jeff Clement <jeffrey.clement@gmail.com>
2026-02-28 09:56:40 -0700

    fix: update Content Security Policy to allow 'unsafe-eval' for scripts

--- internal/server/vhost.go
+++ internal/server/vhost.go
@@ -116,6 +116,6 @@ func (s *Server) serveAdmin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 	w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'")
+	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; base-uri 'self'")
 	s.adminMux.ServeHTTP(w, r)
 }
